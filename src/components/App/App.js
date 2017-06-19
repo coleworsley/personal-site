a8b1b5818190085d1fdcfa5955c6d9e238f23cee
coleworsley <cole.s.worsley@gmail.com>
2017-06-18 21:15:39 -0600

    initialize repository

---
+++ src/components/App/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      page: 'about',
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>About</h2>
+        </div>
+        <p className="App-intro">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
