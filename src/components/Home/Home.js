4d1e6883fadc2ff3142baa626dc085a6330c6109
coleworsley <cole.s.worsley@gmail.com>
2017-07-13 22:46:07 -0600

    format nav bar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,9 +3,8 @@ import React from 'react';
 export const Home = () => {
   return (
     <section className='home'>
-      <h1 className='welcome title'>Cole Worsley</h1>
-      <p className='welcome subtitle'>Front End Developer</p>
-      <button className='welcome contact-button'>Contact</button>
+      <h1 className='home-title'>Cole Worsley</h1>
+      <p className='home-subtitle'>Front End Developer</p>
     </section>
   )
 }
