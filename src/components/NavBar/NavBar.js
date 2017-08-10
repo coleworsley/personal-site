bd7688068fcfd2016246c72bcba557aae97ba948
coleworsley <cole.s.worsley@gmail.com>
2017-08-10 13:00:40 -0600

    add contact and projects component & add link tags

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
   return (
     <nav className='nav-bar'>
       <h1 className='nav-title'>CW</h1>
       <ul className='nav-list'>
-        <li className='nav-link'>Welcome</li>
-        <li className='nav-link'>About</li>
-        <li className='nav-link'>Projects</li>
-        <li className='nav-link'>Contact</li>
+        <li><Link className='nav-link' to='/'>Welcome</Link></li>
+        <li><Link className='nav-link' to='/about'>About</Link></li>
+        <li><Link className='nav-link' to='/projects'>Projects</Link></li>
+        <li><Link className='nav-link' to='/contact'>Contact</Link></li>
       </ul>
     </nav>
   )
