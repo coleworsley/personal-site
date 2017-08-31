088ef4b963a0d911efeb3efc7a08c8e605f94e76
coleworsley <cole.s.worsley@gmail.com>
2017-08-30 23:26:09 -0600

    update nav link class names to BEM

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import './NavBar.css';
 import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
   return (
-    <nav className='nav-bar'>
-      <h1 className='nav-title'>CW</h1>
-      <ul className='nav-list'>
-        <li><Link className='nav-link' to='/'>Welcome</Link></li>
-        <li><Link className='nav-link' to='/about'>About</Link></li>
-        <li><Link className='nav-link' to='/projects'>Projects</Link></li>
-        <li><Link className='nav-link' to='/contact'>Contact</Link></li>
+    <nav className='nav'>
+      <h1 className='nav__title'>CW</h1>
+      <ul className='nav__list'>
+        <li><Link className='nav__link' to='/'>Welcome</Link></li>
+        <li><Link className='nav__link' to='/about'>About</Link></li>
+        <li><Link className='nav__link' to='/projects'>Projects</Link></li>
+        <li><Link className='nav__link' to='/contact'>Contact</Link></li>
       </ul>
     </nav>
   )
