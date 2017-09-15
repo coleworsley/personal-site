3bd9a1aa5c971c62ca3e4fe53bb1d984fc564e50
coleworsley <cole.s.worsley@gmail.com>
2017-09-15 12:47:58 -0600

    add initial contact jsx

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import './Contact.css'
 
 export const Contact = () => {
+
   return (
-    <p>this is the contact tab</p>
+    <div className='contact'>
+      <h1 className='contact-title'>Contact Me</h1>
+      <form id='contact' autoComplete='off'>
+        <input
+          id='name'
+          type='text'
+          placeholder='Name'
+          name='name'/>
+        <input
+          id='email'
+          type='text'
+          placeholder='Email'
+          name='email'/>
+        <input
+          id='subject'
+          type='text'
+          placeholder='Subject'
+          name='subject'/>
+        <textarea
+          name='message'
+          id='message'
+          placeholder='Message'>
+        </textarea>
+      </form>
+    </div>
   )
 }
