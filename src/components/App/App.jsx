6f8c8ad90e0b30b95d26b9ad1b0925568815a7ba
coleworsley <cole.s.worsley@gmail.com>
2017-09-22 14:23:17 -0600

    add linter

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import Canvas from '../Canvas/Canvas';
 import About from '../About/About';
 import { NavBar } from '../NavBar/NavBar';
 import { Home } from '../Home/Home';
-import { Route } from 'react-router-dom';
 import { Contact } from '../Contact/Contact';
 import { Projects } from '../Projects/Projects';
 
@@ -20,10 +20,10 @@ class App extends Component {
       <div className="app">
         <NavBar />
         <Canvas />
-        <Route exact path='/' component={Home}/>
-        <Route exact path='/about' component={About}/>
-        <Route exact path='/projects' component={Projects}/>
-        <Route exact path='/contact' component={Contact}/>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/projects" component={Projects}/>
+        <Route exact path="/contact" component={Contact}/>
       </div>
     );
   }
