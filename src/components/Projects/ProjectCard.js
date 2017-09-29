0fbc9a51db8a7c55bb031a9da5915bcb0cbcd628
coleworsley <cole.s.worsley@gmail.com>
2017-09-29 13:27:40 -0600

    add sample project

---
+++ src/components/Projects/ProjectCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProjectCard = (props) => {
+  return (
+    <div className="project-card">
+      <h3 className="project-card__title">Title</h3>
+      <p className="project-card__content">Content</p>
+    </div>
+  );
+};
+
+
+export default ProjectCard;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import ProjectCard from './ProjectCard';
+
+const projectArray = [
+  {
+    title: 'Test',
+    content: 'This is the content',
+  },
+  {
+    title: 'Test',
+    content: 'This is the content',
+  },
+];
 
 const Projects = () => {
+  const projectCards = projectArray.map(project => <ProjectCard {...project} />);
   return (
     <div className="projects tab">
-      <p>this is the projects tab</p>
+      { projectCards }
     </div>
   );
 };
