a4f9f4ea6b60ec3532c97ac6c882df96431ef626
coleworsley <cole.s.worsley@gmail.com>
2017-10-09 09:57:21 -0600

    add multiple projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,49 +1,7 @@
 import React from 'react';
 import ProjectCard from './ProjectCard';
 import './Projects.css';
-
-const projectArray = [
-  {
-    id: 'movie-tracker',
-    title: {
-      text: 'Movie',
-      span: 'Tracker',
-    },
-    description: 'A minature clone of IMDB utilizing the movie database API',
-    link: 'https://github.com/coleworsley/movie-tracker',
-    image: './assets/movie-tracker.png',
-  },
-  {
-    id: 'jet-fuel',
-    title: {
-      text: 'Jet',
-      span: 'Fuel',
-    },
-    description: '',
-    link: 'https://github.com/coleworsley/jet-fuel',
-    image: './assets/jet-fuel.png',
-  },
-  {
-    id: 'happy-hour-power',
-    title: {
-      text: 'Happy Hour',
-      span: 'Power',
-    },
-    description: '',
-    link: 'https://github.com/coleworsley/happy-hour-power',
-    image: './assets/happy-hour-power.png',
-  },
-  {
-    id: 'comp-challenge',
-    title: {
-      text: 'Comp',
-      span: 'Challenge',
-    },
-    description: '',
-    link: 'https://github.com/coleworsley/',
-    image: './assets/comp-challenge.png',
-  },
-];
+import { projectArray } from '../../utils/constants';
 
 const Projects = () => {
   const projectCards = projectArray.map(project => <ProjectCard {...project} />);
---
+++ src/utils/constants.js
@@ -0,0 +1,62 @@
+export const projectArray = [
+  {
+    id: 'movie-tracker',
+    title: {
+      text: 'Movie',
+      span: 'Tracker',
+    },
+    description: 'A minature clone of IMDB utilizing the movie database API',
+    link: 'https://github.com/coleworsley/movie-tracker',
+    image: './assets/movie-tracker.png',
+  },
+  {
+    id: 'jet-fuel',
+    title: {
+      text: 'Jet',
+      span: 'Fuel',
+    },
+    description: '',
+    link: 'https://github.com/coleworsley/jet-fuel',
+    image: './assets/jet-fuel.png',
+  },
+  {
+    id: 'happy-hour-power',
+    title: {
+      text: 'Happy Hour',
+      span: 'Power',
+    },
+    description: '',
+    link: 'https://github.com/coleworsley/happy-hour-power',
+    image: './assets/happy-hour-power.png',
+  },
+  {
+    id: 'comp-challenge-3',
+    title: {
+      text: 'Comp',
+      span: 'Challenge',
+    },
+    description: '',
+    link: 'https://github.com/coleworsley/',
+    image: './assets/comp-challenge-3.png',
+  },
+  {
+    id: 'pattrn-party',
+    title: {
+      text: 'Pattrn',
+      span: 'Party',
+    },
+    description: '',
+    link: 'https://github.com/coleworsley/',
+    image: './assets/pattrn-party.png',
+  },
+  {
+    id: 'swapi-box',
+    title: {
+      text: 'Swapi',
+      span: 'Box',
+    },
+    description: '',
+    link: 'https://github.com/coleworsley/',
+    image: './assets/swapi-box.png',
+  },
+];
